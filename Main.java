0decb9d1a7a2c815348b7a1a5e420a78500dbde2
Honest161 <154550126+Honest161@users.noreply.github.com>
2024-01-12 13:54:18 -0500

    Added wasd inputs to main

--- Main.java
+++ Main.java
@@ -1,11 +1,41 @@
 //Create frame/panel/drawing panel
 //collect input
 //draw everything
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("main");
-    }
-
+	static boolean w,a,s,d;
+	public static void main(String[] args){
+	    this.addKeyListener(new KeyAdapter() {
+	    	public void keyPressed(KeyEvent e) {
+	    		int keyCode = e.getKeyCode();
+	    	    if(keyCode == 'W' || keyCode == 38) w = true;
+	    	    if(keyCode == 'A' || keyCode == 37) a = true;
+	    	    if(keyCode == 'S' || keyCode == 40) s = true;
+	    	    if(keyCode == 'D' || keyCode == 39) d = true; 
+	    	}
+	       	public void keyReleased(KeyEvent e) {
+	       		int keyCode = e.getKeyCode();
+	       		if(keyCode == 'W' || keyCode == 38) w = false;
+	       		if(keyCode == 'A' || keyCode == 37) a = false;
+	       		if(keyCode == 'S' || keyCode == 40) s = false;
+	       		if(keyCode == 'D' || keyCode == 39) d = false;          
+	    	}
+	    });
+  }
 }
+
+/*
+frame.addMouseListener(new MouseAdapter() {
+    public void mouseClicked(MouseEvent e) { 
+        if(e.getButton() == MouseEvent.BUTTON1) {
+            shoot()
+        }
+        if(e.getButton() == MouseEvent.BUTTON3) {
+            bomb()
+        }
+    }
+});
+*/
