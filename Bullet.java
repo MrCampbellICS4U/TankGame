95729c2514ce3b636d28ba35f7906e56f873398c
BigSoupMan <154549581+BigSoupMan@users.noreply.github.com>
2024-01-12 17:58:52 -0500

    Bullets be shoot'n

--- Bullet.java
+++ Bullet.java
@@ -1,39 +1,34 @@
+
 //Create bullet objects via Tank.shoot()
 import java.awt.Rectangle;
 import java.util.Random;
+
 public class Bullet {
+	final int WIDTH = 10;
+	final int HEIGHT = 10;
 	Random ra = new Random();
 	double x, y;
-	int speedx = 5;
-	int speedy = 5;
-	int width = 10;
-	int height = 10;
+	double speedx = 5;
+	double speedy = 5;
+	int bounces;
 	Rectangle hitbox;
-	Bullet(double bx, double by){
-		 x = bx;
-		 y = by;
-		hitbox = new Rectangle((int)x, (int)y, width, height);
-	 }
-	public void shoot(int panW, int panH) {
-//		if (count > maxFire) return;
-			Bullet b = new Bullet (ra.nextInt(200, 401), ra.nextInt(200, 401));
-			Main.bullets.add(b);
-				if (y >= panH - height || y <= 0) {
-					speedy *= -1; 
-					y = 300;
-					System.out.print(speedy);
-				}
-				if (x >= (panW - width) || x <= 0) {
-					 speedx *= -1;
-					 x = 300;
-					 System.out.println(speedx);
-				}
-				y += speedy;
-				x += speedx;
-			
-		/* **NOTE**
-		 * This method is to be used once main class and tank class are created
-		 * panW and panH will not be parameters in the future
-		 */
+
+	Bullet(double bx, double by, double xspeed, double yspeed, int bounce) {
+		x = bx;
+		y = by;
+		speedx = xspeed;
+		speedy = yspeed;
+		bounces = bounce;
+		hitbox = new Rectangle((int) x, (int) y, WIDTH, HEIGHT);
+	}
+
+	public void move(int panW, int panH) {
+		if (y >= panH - HEIGHT || y <= 0)
+			speedy *= -1;
+		if (x >= panW - WIDTH || x <= 0)
+			speedx *= -1;
+
+		y += speedy;
+		x += speedx;
 	}
 }
--- Tank.java
+++ Tank.java
@@ -1,6 +1,22 @@
 //Create player & enemy tank objects
 //move(), shoot()
 
-public class Tank {
+import java.util.ArrayList;
 
+public class Tank {
+    ArrayList<Bullet> bullets = new ArrayList<Bullet>();
+    double x, y, TEMP;
+    int bounces;
+
+    /**
+     * Create new Bullet and add it to array of Bullets
+     */
+    public void shoot(int count, int maxFire, boolean shoot) {
+        if (count == maxFire)
+            return;
+        if (shoot) {
+            bullets.add(new Bullet(x, y, TEMP, TEMP, bounces));
+            shoot = false;
+        }
+    }
 }
