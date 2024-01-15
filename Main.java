7149ca59a8c80785c7ec5de180de74702b560d5f
Will <73199553+willc737@users.noreply.github.com>
2024-01-15 12:47:44 -0500

    basics

--- Main.java
+++ Main.java
@@ -2,10 +2,45 @@
 //collect input
 //draw everything
 
-public class Main {
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    int screenHeight, screenWidth;
+
+    DrawingPanel drawing = new DrawingPanel();
 
     public static void main(String[] args) {
-        System.out.println("main");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main();
+            }
+        });
+    }
+
+    Main() {
+        setTitle("Tank Game");
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setResizable(false);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setUndecorated(true);
+        setVisible(true);
+        screenHeight = getHeight();
+        screenWidth = getWidth();
+        add(drawing);
+        drawing.setBounds(0, 0, screenWidth, screenHeight);
+    }
+
+    private class DrawingPanel extends JPanel {
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, screenWidth - 20, screenHeight - 20);
+        }
     }
 
 }
