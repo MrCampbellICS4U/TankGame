d41703e94c0efad39d76035838bb8806a8852138
Honest161 <154550126+Honest161@users.noreply.github.com>
2024-01-19 13:33:37 -0500

    Update Tank.java

--- Tank.java
+++ Tank.java
@@ -3,4 +3,38 @@
 
 public class Tank {
 
+<<<<<<< Updated upstream
 }
+=======
+public class Tank extends Rectangle {
+    ArrayList<Bomb> bombs = new ArrayList<Bomb>();
+    ArrayList<Bullet> bullets = new ArrayList<Bullet>();
+    double x, y;
+    int bounces;
+    int max;
+
+    Tank(int x, int y, int width, int height, int bounces, int max) {
+        super(x, y, width, height);
+        this.x = x;
+        this.y = y;
+        this.bounces = bounces;
+        this.max = max;
+    }
+
+    /**
+     * Create new Bullet and add it to array of Bullets
+     */
+    public void shoot(double dx, double dy) {
+        // if (bullets.size() < max) {
+        bullets.add(new Bullet(x, y, dx, dy, bounces, this));
+        // }
+    }
+
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+        super.x = (int) x;
+        super.y = (int) y;
+    }
+}
+>>>>>>> Stashed changes
